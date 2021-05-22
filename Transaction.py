dc7c56ad8c8a9f74e70f25e37fb58da6e2a012a6
Alon Ben Dov <alonbd21@gmail.com>
2021-05-22 15:11:05 +0300

    Transaction and User improvements and documentations. Crypto.py removed

--- Transaction.py
+++ Transaction.py
@@ -1,27 +1,80 @@
 from hashlib import sha256
+from User import User
+import time
+
 
 class Transaction:
+    SEP = ','
 
-    def __init__(self, sender, receiver, amount):
+    def __init__(self, sender, receiver, amount, time, signature=None):
+        """
+        Creates new Transaction
+        :param sender: who sends the money, verifying key, bytes
+        :param receiver: who receives the money, verifying key, bytes
+        :param amount: how much money to transfer
+        :param signature: digital signature of the sender
+        """
         self.__sender = sender
         self.__receiver = receiver
         self.__amount = amount
-        self.__signature = None
+        self.__time = time
+        self.__signature = signature
 
-    def __init__(self, string):
-        data = string.split(',')
-        self.__sender, self.__reciever, self.__amount, self.__signature = data
+    @classmethod
+    def from_string(cls, string):
+        """
+        Creates transaction from string
+        :param string: string in the format of __str__
+        :return: new Transaction
+        """
+        fields = string.split(cls.SEP)
+        signature = None
+        if len(fields) == 5:
+            signature = bytes(fields[4])
+        return Transaction(bytes(fields[0]), bytes(fields[1]),
+                           float(fields[2]), int(fields[3]), signature)
 
     def __str__(self):
-        fields = [str(self.__sender),str(self.__reciever), str(self.__amount), str(self.__signature)]
-        return ','.join(fields)
-
+        """
+        return string with the data of the transaction
+        """
+        fields = [str(self.__sender), str(self.__receiver), str(self.__amount),
+                  str(int(self.__time)), str(self.__signature)]
+        return self.SEP.join(fields)
 
     def sign(self, user):
-        ecdsa
+        """
+        puts signature on the transaction with the credentials on a given user
+        :param user: credentials of the sender, User
+        :return: signature, bytes
+        """
+        self.__signature = user.sign(self.__sign_on())
+
+    def __sign_on(self):
+        """
+        :return: bytes that represents the data of the transaction to be signed
+        """
+        h1 = sha256(self.__sender).digest()
+        h2 = sha256(self.__receiver).digest()
+        h3 = sha256(bytes(str(self.__amount), encoding="ascii")).digest()
+        h4 = sha256(bytes(self.__time)).digest()
+        return sha256(h1 + h2 + h3 + h4).digest()
+
+    def verify(self):
+        """
+        verifies the signature with the data of the transaction.
+        :return: True if the signature is valid, False else.
+        """
+        if self.__signature is None:
+            return False
+        return User.verify(self.__sign_on(), self.__sender, self.__signature)
 
 
-    def __hash__(self):
-        sha256().digest()
-
-
+if __name__ == '__main__':
+    alice = User.generate()
+    bob = User.generate()
+    trans = Transaction(alice.get_vk_bytes(), bob.get_vk_bytes(), 100.5,
+                        int(time.time()))
+    trans.sign(alice)
+    print(trans)
+    print(trans.verify())
--- User.py
+++ User.py
@@ -5,37 +5,75 @@ CURVE = NIST256p
 
 class User:
     def __init__(self, sk, vk):
+        """
+        :param sk: signing (private) key
+        :type sk: ecdsa.SigningKey
+        :param vk: verifying (public) key
+        :type vk: ecdsa.VerifyingKey
+        """
         self.__sk = sk
         self.__vk = vk
 
     @classmethod
     def generate(cls):
+        """
+        Creates new user with random sk and vk
+        :return: User
+        """
         sk = SigningKey.generate(curve=CURVE)
         return cls(sk, sk.verifying_key)
 
     @classmethod
     def from_sk_bytes(cls, sk):
+        """
+        Creates new user from a given signing (private) key
+        :param sk: signing key
+        :type sk: bytes
+        :return: User
+        """
         sk = SigningKey.from_string(sk, curve=CURVE)
         return cls(sk, sk.verifying_key)
 
     @classmethod
-    def verify(cls, data_bits, vk, signature):
+    def verify(cls, data_bytes, vk, signature):
+        """
+        Verifies a signature
+        :param data_bytes: bytes of the signed data
+        :param vk: verifying key (bytes)
+        :param signature: signature (bytes)
+        :return:
+        """
         vk = VerifyingKey.from_string(vk, curve=CURVE)
-        return vk.verify(signature, data_bits)
+        return vk.verify(signature, data_bytes)
 
-    def get_sk_vk_bytes(self):
-        return self.__sk.to_string(), self.__vk.to_string()
+    def get_sk_bytes(self):
+        """
+        :return: signing key (bytes)
+        """
+        return self.__sk.to_string()
+
+    def get_vk_bytes(self):
+        """
+        :return: verifying key (bytes)
+        """
+        return self.__vk.to_string()
 
     def __repr__(self):
         return "USER\nsk={0}\nvk={1}".format(self.__sk.to_string(),
                                              self.__vk.to_string())
 
-    def sign(self, data_bits):
-        return self.__sk.sign(data_bits)
+    def sign(self, data_bytes):
+        """
+        sign data with the signing key
+        :param data_bytes: data to sign on (bytes)
+        :return: signature (bytes)
+        """
+        return self.__sk.sign(data_bytes)
+
 
 if __name__ == "__main__":
     a = User.generate()
-    b = User.from_sk_bytes(a.get_sk_vk_bytes()[0])
+    b = User.from_sk_bytes(a.get_sk_bytes())
     print(repr(a))
     print(repr(b))
     print(repr(a) == repr(b))
@@ -46,5 +84,4 @@ if __name__ == "__main__":
 
     print(signature)
 
-    assert User.verify(data,b.get_sk_vk_bytes()[1],signature)
-
+    assert User.verify(data, b.get_vk_bytes(), signature)
