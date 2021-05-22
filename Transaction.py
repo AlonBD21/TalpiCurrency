8c33cbf7f7765915d5502411821ea7c344dd794e
Alon Ben Dov <alonbd21@gmail.com>
2021-05-22 13:28:02 +0300

    User class is ready (sk, vk, ecdsa)

--- Transaction.py
+++ Transaction.py
@@ -1,21 +1,27 @@
-import ecdsa
 from hashlib import sha256
 
-
 class Transaction:
-    def __init__(self, sender, reciever, amount):
-        self.sender = sender
-        self.reciever = reciever
-        self.amount = amount
 
-    def sign_material(self):
-        return sha256(sha256(self.sender).digest() + sha256(
-            self.reciever).digest() + sha256(self.amount).digest()).digest()
+    def __init__(self, sender, receiver, amount):
+        self.__sender = sender
+        self.__receiver = receiver
+        self.__amount = amount
+        self.__signature = None
+
+    def __init__(self, string):
+        data = string.split(',')
+        self.__sender, self.__reciever, self.__amount, self.__signature = data
+
+    def __str__(self):
+        fields = [str(self.__sender),str(self.__reciever), str(self.__amount), str(self.__signature)]
+        return ','.join(fields)
 
 
-    def sign(self, sk):
-        self.sign = ecdsa.sbytes(self)
-        sk.sign()
+    def sign(self, user):
+        ecdsa
 
-    def verify(self, sig, pk):
-        return pk.verify(sig, sign_material())
+
+    def __hash__(self):
+        sha256().digest()
+
+
--- User.py
+++ User.py
@@ -1,1 +1,50 @@
+from ecdsa import SigningKey, VerifyingKey, NIST256p
+
+CURVE = NIST256p
+
+
 class User:
+    def __init__(self, sk, vk):
+        self.__sk = sk
+        self.__vk = vk
+
+    @classmethod
+    def generate(cls):
+        sk = SigningKey.generate(curve=CURVE)
+        return cls(sk, sk.verifying_key)
+
+    @classmethod
+    def from_sk_bytes(cls, sk):
+        sk = SigningKey.from_string(sk, curve=CURVE)
+        return cls(sk, sk.verifying_key)
+
+    @classmethod
+    def verify(cls, data_bits, vk, signature):
+        vk = VerifyingKey.from_string(vk, curve=CURVE)
+        return vk.verify(signature, data_bits)
+
+    def get_sk_vk_bytes(self):
+        return self.__sk.to_string(), self.__vk.to_string()
+
+    def __repr__(self):
+        return "USER\nsk={0}\nvk={1}".format(self.__sk.to_string(),
+                                             self.__vk.to_string())
+
+    def sign(self, data_bits):
+        return self.__sk.sign(data_bits)
+
+if __name__ == "__main__":
+    a = User.generate()
+    b = User.from_sk_bytes(a.get_sk_vk_bytes()[0])
+    print(repr(a))
+    print(repr(b))
+    print(repr(a) == repr(b))
+
+    data = b"Alon Ben Dov"
+
+    signature = a.sign(data)
+
+    print(signature)
+
+    assert User.verify(data,b.get_sk_vk_bytes()[1],signature)
+
