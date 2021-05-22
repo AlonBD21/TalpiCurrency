ac1c3dc159e3c6644e6c8f3b92692520e4f9ef52
shacharaviram <shahar.aviram@mail.huji.ac.il>
2021-05-22 13:25:21 +0300

    ll

--- Block.py
+++ Block.py
@@ -1,8 +1,36 @@
 import time
+from hashlib import sha256
+
 from Header import *
 from Transaction import *
 
+
 class Block:
 
-    def _init__(self, prev_block):
-        self.header = Header.header(prev_block.prev_hash, prev_block.n_bits)
+    def __init__(self, nonce=0, prev_block=None, transactions="0" * 32):
+        if prev_block:
+            prev_hash = hash (prev_block)
+        else:
+            prev_hash = "0" * 32
+        self.header = Header (prev_hash, transactions, nonce)
+        self.transactions = transactions
+
+    def __str__(self):
+        s = str (self.header)
+        for t in self.transactions:
+            s += "," + str (t)
+        return s
+
+    def is_solved(self):
+        hashed = ''.join (format (x, '08b') for x in self.hash ())
+        for i in range (self.header.n_bits):
+            if hashed[i]:
+                return False
+        return True
+
+    def __hash__(self):
+        h1 = sha256 (self.header).digest ()
+        h2 = sha256 (self.transactions).digest ()
+        hashed = sha256 (h1 + h2).digest ()
+        return hashed
+        # ''.join (format (x, '08b') for x in hashed)  if we want to make it binary
--- Header.py
+++ Header.py
@@ -1,25 +1,45 @@
-import time
-from hashlib import sha256
+from time import time
+from datetime import datetime
 
-class header:
-    def __init__(self, prev_hash, n_bits, nonce):
+class Header:
+    def __init__(self, prev_hash, transactions, nonce, n_bits=10):
         self.prev_hash = prev_hash
-        # self.root_hash = Merkle.calc_root()
-        self.root_hash = 
-        self.time = time.time()
+        self.create_merkle_root(transactions)
+        self.time = int(time())
         self.n_bits = n_bits
         self.nonce = nonce
 
-    
     def create_merkle_root(self, transactions):
+        transactions = self.hash_all(transactions)
         width_of_tree = len (transactions)
         while width_of_tree > 1:
             hashes = []
             for i in range(0,width_of_tree,2):
                 if i == len (transactions) - 1:
-                    hashes.append (hash (transactions[i], transactions[i]))
+                    hashes.append (hash(transactions[i]), transactions[i])
                 else:
                     hashes.append (hash (transactions[i], transactions[i + 1]))
             width_of_tree = len (hashes)
             transactions = hashes
         self.root_hash = transactions[0]
+
+    def hash_all(self, transactions):
+        hashed = []
+        for trans in transactions:
+            hashed.append(hash(trans))
+        return hashed
+
+    def __str__(self):
+        return str(self.prev_hash)+","+str(self.root_hash)+","+str(self.time)+","+str(self.n_bits)+","+str(self.nonce)
+
+    def __repr__(self):
+        return "previous hash: "+str(self.prev_hash)+"\nmerkle hash: "+str(self.root_hash)+"\ntime: "+\
+               str(datetime.fromtimestamp(self.time))+"\ntarget: "+str(self.n_bits)+"\nnonce: "+str(self.nonce)
+
+# header1 = Header("23567fac2332cd2312be2",["Shachar,Alon,100"],nonce=874561)
+# print(repr(header1))
+# print(header1)
+
+# t=time()
+# p = datetime.fromtimestamp(int(t))
+# print(p)
