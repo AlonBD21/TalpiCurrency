e102cc90182ca1de67b6cc7a867ff6977d29feb8
shacharaviram <shahar.aviram@mail.huji.ac.il>
2021-05-24 01:01:14 +0300

    ll

--- Client.py
+++ Client.py
@@ -3,13 +3,10 @@ import sys
 from time import sleep
 
 from Transaction import *
-
+# setup
 interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
-allips = [ip[-1][0] for ip in interfaces]
-print(interfaces)
-
-List = []
-
+all_ips = [ip[-1][0] for ip in interfaces]
+PORT = 5005
 
 msg = b'hello world'
 print(msg)
@@ -17,13 +14,14 @@ print(type(msg))
 
 while True:
 
-    for ip in allips:
+    for ip in all_ips:
         print(f'sending on {ip}')
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.bind((ip,0))
-        sock.sendto(msg, ("255.255.255.255", 5005))
+        sock.sendto(msg, ("255.255.255.255", PORT))
         sock.close()
+    msg = bytes (input (), "utf-8")
 
     sleep(2)
 
--- Server.py
+++ Server.py
@@ -2,12 +2,12 @@ import ecdsa
 import socket
 from hashlib import sha256
 
-from BlockChain import *
-from Block import *
-from Client import *
+# from BlockChain import *
+# from Block import *
+# from Client import *
 
-IP = '192.168.137.246'
-PORT = 1110
+IP = '132.64.143.111'
+PORT = 5005
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = (IP, PORT)
@@ -16,9 +16,10 @@ s.bind(server_address)
 while True:
     data, address = s.recvfrom(4096)
     data.decode('utf-8')
+    print(data)
     ##Verify
 
-    send_data = "Transaction Failed"
+    send_data = "Transaction Succeeded"
     ##Answer sender
     s.sendto(send_data.encode('utf-8'), address)
     print("\n\n 1. Server sent : ", send_data,"\n\n")
